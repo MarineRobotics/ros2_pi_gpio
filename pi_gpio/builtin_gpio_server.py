d9d5781a472aae065fd0567548d6146ada2fc294
Vincent Vandyck <vincent.vandyck@gmail.com>
2024-07-25 18:45:33 +0000

    simplified pcf, made more like builtin

--- pi_gpio/builtin_gpio_server.py
+++ pi_gpio/builtin_gpio_server.py
@@ -1,5 +1,7 @@
 import time
 import RPi.GPIO as GPIO
+from rclpy.node import Node
+from std_msgs.msg import Bool
 from mr_interfaces.action import GPIO as GPIO_Action
 from .base_gpio_action_server import BaseGPIOActionServer
 
@@ -8,28 +10,42 @@ class BuiltinGPIOActionServer(BaseGPIOActionServer):
         super().__init__('pi_gpio_server', 'pi_gpio_server', config_file)
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
-        self.pin_types = {}  # Store pin types (in/out) instead of RaspberryPIGPIO objects
+        self.pin_publishers = {}
         self.setup_pins()
+        
+        # Create a timer that calls the publish_pin_states method 10 times per second
+        self.create_timer(0.1, self.publish_pin_states)
 
     def setup_pin(self, pin_id, pin_type):
+        pin_id = int(pin_id)
         if pin_type == "in":
             GPIO.setup(pin_id, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            self.pin_publishers[pin_id] = self.create_publisher(Bool, f'pin_state_{pin_id}', 1)
         elif pin_type == "out":
             GPIO.setup(pin_id, GPIO.OUT)
         self.pin_types[pin_id] = pin_type
+
+    def publish_pin_states(self):
+        for pin_id, pin_type in self.pin_types.items():
+            if pin_type == "in":
+                state = GPIO.input(pin_id)
+                self.pin_publishers[pin_id].publish(Bool(data=bool(state)))
 
     def perform_gpio_action(self, pin_id, action_type):
         result = GPIO_Action.Result()
         pin_id = int(pin_id)
 
-        if action_type == "high" and self.pin_types[pin_id] == "out":
+        if pin_id not in self.pin_types or self.pin_types[pin_id] != "out":
+            self.get_logger().warn(f'Invalid output pin {pin_id}')
+            result.value = -1
+            return result.value
+
+        if action_type == "high":
             GPIO.output(pin_id, GPIO.HIGH)
-            time.sleep(0.1)
-            result.value = 3
-        elif action_type == "low" and self.pin_types[pin_id] == "out":
+            result.value = 1
+        elif action_type == "low":
             GPIO.output(pin_id, GPIO.LOW)
-            time.sleep(0.1)
-            result.value = 3
+            result.value = 0
         elif action_type == "read":
             result.value = GPIO.input(pin_id)
         else:
--- pi_gpio/pcf8574_gpio_server.py
+++ pi_gpio/pcf8574_gpio_server.py
@@ -1,90 +1,54 @@
-import rclpy
-from rclpy.node import Node
-from rclpy.action import ActionServer
-from rclpy.callback_groups import ReentrantCallbackGroup
-from rclpy.executors import MultiThreadedExecutor
-from std_msgs.msg import Bool
-from mr_interfaces.action import GPIO
 import board
 import adafruit_pcf8574
 import digitalio
-import time
+from mr_interfaces.action import GPIO as GPIO_ACTION
+from .base_gpio_action_server import BaseGPIOActionServer
 
-class PCF8574IO(Node):
+class PCF8574IO(BaseGPIOActionServer):
     def __init__(self, config_file="pcf8574_config.txt"):
-        super().__init__('pcf8574_io')
+        super().__init__('pcf8574_io', 'pcf8574_gpio_server', config_file)
         
         # Initialize I2C and PCF8574
         i2c = board.I2C()
         self.pcf = adafruit_pcf8574.PCF8574(i2c)
         
-        # Read configuration
-        self.input_pins, self.output_pins = self.read_config(config_file)
-        
-        # Set up input pins and create publishers
-        self.pin_publishers = {}
-        
-        for pin, topic in self.input_pins.items():
-            pin_obj = self.pcf.get_pin(pin)
-            pin_obj.switch_to_input(pull=digitalio.Pull.UP)
-            self.pin_publishers[pin] = self.create_publisher(Bool, f'{topic}', 1)
-        
-        # Set up output pins
-        for pin in self.output_pins:
-            pin_obj = self.pcf.get_pin(pin)
-            pin_obj.switch_to_output(value=False)
+        self.setup_pins()
         
         # Create a timer that calls the publish_pin_states method 10 times per second
         self.create_timer(0.1, self.publish_pin_states)
-        
-        # Set up action server for output control
-        self._action_server = ActionServer(
-            self,
-            GPIO,
-            'pcf8574_gpio_server',
-            self.execute_callback,
-            callback_group=ReentrantCallbackGroup())
 
-    def read_config(self, config_file):
-        input_pins = {}
-        output_pins = []
-        try:
-            with open(config_file, 'r') as f:
-                for line in f:
-                    pin, pin_type, topic = line.strip().split(',')
-                    pin = int(pin)
-                    if pin_type.lower() in ['input', 'in']:
-                        # Add topic and pin to input pin dictionary
-                        input_pins[pin] = topic
-                    elif pin_type.lower() in ['output', 'out']:
-                        output_pins.append(pin)
-                    else:
-                        self.get_logger().warn(f'Invalid pin type {pin_type} for pin {pin}')
-        except FileNotFoundError:
-            self.get_logger().error(f'Config file {config_file} not found')
-            raise
-        return input_pins, output_pins
+    def setup_pin(self, pin_id, pin_type):
+        pin_id = int(pin_id)
+        pin_obj = self.pcf.get_pin(pin_id)
+        
+        if pin_type == "in":
+            pin_obj.switch_to_input(pull=digitalio.Pull.UP)
+            self.pin_publishers[pin_id] = self.create_publisher(GPIO_ACTION, f'external_gpio_{pin_id}', 1)
+        elif pin_type == "out":
+            pin_obj.switch_to_output(value=False)
+        
+        self.pin_types[pin_id] = pin_type
 
     def publish_pin_states(self):
-        for pin in self.input_pins:
-            pin_obj = self.pcf.get_pin(pin)
-            state = not pin_obj.value  # Invert because input is pulled up
-            self.pin_publishers[pin].publish(Bool(data=state))
+        for pin_id, pin_type in self.pin_types.items():
+            if pin_type == "in":
+                pin_obj = self.pcf.get_pin(pin_id)
+                state = not pin_obj.value  # Invert because input is pulled up
+                msg = GPIO_ACTION()
+                msg.gpio = f"{pin_id},{int(state)}"
+                self.pin_publishers[pin_id].publish(msg)
 
-    def execute_callback(self, goal_handle):
-        request = goal_handle.request
-        pin_id, action_type = request.gpio.split(',')
+    def perform_gpio_action(self, pin_id, action_type):
         pin_id = int(pin_id)
-        
-        result = GPIO.Result()
-        
-        if pin_id not in self.output_pins:
+        result = GPIO_ACTION.Result()
+
+        if pin_id not in self.pin_types or self.pin_types[pin_id] != "out":
             self.get_logger().warn(f'Invalid output pin {pin_id}')
-            goal_handle.abort()
+            result.value = -1
             return result
-        
+
         pin = self.pcf.get_pin(pin_id)
-        
+
         if action_type == "high":
             pin.value = True
             result.value = 1
@@ -95,8 +59,10 @@ class PCF8574IO(Node):
             result.value = int(not pin.value)  # Invert the value for consistency with input pins
         else:
             self.get_logger().warn(f'Invalid action {action_type} for pin {pin_id}')
-            goal_handle.abort()
-            return result
-        
-        goal_handle.succeed()
-        return result
+            result.value = -1
+
+        return result.value  # Return just the value, not the whole result object
+
+    def destroy(self):
+        # Perform any necessary cleanup
+        super().destroy()
